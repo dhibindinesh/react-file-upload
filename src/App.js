34d37b2eba3cb7aa6d0d5a1d9473d80b1226f7e6
Dhibin Dinesh <dhibin@agaze.in>
2019-10-04 17:37:40 +0530

    url input added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,28 @@ class App extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        selectedFile: null
+        selectedFile: null,
+        url: ''
       }
   };
+  urlChangeHandler=event=>{
+    // this.setState({
+    //   url: event.value
+    // });
+    console.log(event)
+    this.state.url = document.getElementById('url').value
+
+    console.log('state ::: ',this.state);
+  //   console.log(this.validateSize(event));
+  //   if(this.validateSize(event)){ 
+  //     console.log(file);
+  // // if return true allow to setState
+  //    this.setState({
+  //     selectedFile: file
+  //     });
+
+  //   }
+  }
   onChangeHandler=event=>{
     var file = event.target.files[0];
     console.log(file);
@@ -26,14 +45,14 @@ class App extends Component {
   }
   fileUploadHandler = () => {
     const data = new FormData()
-    console.log(this.state.selectedFile);
     data.append('file', this.state.selectedFile)
-    console.log(data);
-    axios.post("http://localhost:8010/api/v1/upload", data)
+    axios.post(this.state.url, data)
       .then(res => { // then print response status
+        console.log(res)
         toast.success('upload success')
       })
       .catch(err => { // then print response status
+        console.log(err)
         toast.error('upload fail')
       })
 
@@ -55,16 +74,20 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-              <ToastContainer />
+            <ToastContainer />
               <form method="post" action="#" id="#">
-                    <div className="form-group files">
-                      <label>Upload Your File </label>
-                      <input type="file" name="file" className="form-control" onChange={this.onChangeHandler}/>
-                    </div>
-                    <div className="col-md-6 pull-right">
-                    <button width="100%" type="button" className="btn btn-info" onClick={this.fileUploadHandler}>Upload File</button>
-                    </div>
-                </form>
+                  <div className="form-group">
+                    <label>URL</label>
+                    <input type="text" name="url" id="url" className="form-control" onChange={this.urlChangeHandler}></input>
+                  </div>
+                  <div className="form-group files">
+                    <label>Upload Your File </label>
+                    <input type="file" name="file" className="form-control" onChange={this.onChangeHandler}/>
+                  </div>
+                  <div className="col-md-6 pull-right">
+                  <button width="100%" type="button" className="btn btn-info" onClick={this.fileUploadHandler}>Upload File</button>
+                  </div>
+              </form>
           </div>
         </div>
       </div>
